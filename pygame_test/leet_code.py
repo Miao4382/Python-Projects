64a77d5badae14d9d140b606526be1fc7fad50b1
Miao4382 <33183267+Miao4382@users.noreply.github.com>
2019-07-31 19:01:05 -0400

    7/31 leetcode notes

--- pygame_test/leet_code.py
+++ pygame_test/leet_code.py
@@ -1,15 +1,25 @@
-import itertools
-
-
 class Solution:
-    def addStrings(self, num1, num2):
-        z = itertools.zip_longest(num1[::-1], num2[::-1], fillvalue='0')
-        res, carry, zero2 = [], 0, 2 * ord('0')
-        for i in z:
-            cur_sum = ord(i[0]) + ord(i[1]) - zero2 + carry
-            res.append(str(cur_sum % 10))
-            carry = cur_sum // 10
-        return ('1' if carry else '') + ''.join(res[::-1])
+    def toGoatLatin(self, S: str) -> str:
+        words = S.split()
+
+        vowel = ('a', 'e', 'i', 'o', 'u')
+
+        for i in range(len(words)):
+
+            if words[i][0].lower() in vowel:
+                words[i] += "ma"
+            else:
+                words[i] = words[i][1:] + words[i][0] + "ma"
+
+            for j in range(i + 1):
+                words[i] += 'a'
+
+            words[i] += ' '
+
+        words[-1] = words[-1][:-1]
+
+        return ''.join(words)
 
 
-print(Solution.addStrings('123', '456'))
+sol = Solution()
+print(sol.toGoatLatin("I speak Goat Latin"))
